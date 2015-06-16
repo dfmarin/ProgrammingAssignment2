dd57953d04c5cc8acf18f4a6f7266c3a15cacecd
Diego Fernando Marin <dfmarin@gmail.com>
2015-06-16 16:09:52 -0500

    added makeCacheMatrix() following makeVector() example

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,19 @@
 ## makeCacheMatrix(): returns a list of functions used to 
 ## set/get the matrix, and set/get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## the cached version of inverse of the matrix
+    inv <- NULL
+    ## set/get for the matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() { x }
+    ## set/get for the inverse of the matrix
+    setinverse <- function(inverse) { inv <<- inverse }
+    getinverse <- function() { inv }
+    # Return a list with defined functions
+    list( set=set, get=get, setinverse=setinverse, getinverse=getinverse )
 }
 
 ## cacheSolve(): returns the inverse of the matrix.
